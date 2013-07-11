faf9b5c635ceac2e91c342120b8ba4e624c5c768
david cieslak <david.cieslak@aunalytics.com>
2013-07-11 14:18:31 +0000

    Successfully added a command

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+echo $HOME
+
 echo "Naming machine $1"
+rm $HOME/.bashrc
 echo "source \$HOME/Base/config/bash/bashrc" >> $HOME/.bashrc
 echo "source \$HOME/Base/config/bash/bash_aliases" >> $HOME/.bashrc
+echo "source \$HOME/Base/config/bash/bash_commands" >> $HOME/.bashrc
 echo "export PS1=[$1]\$PS1" >> $HOME/.bashrc
 echo "source \$HOME/Base/config/vim/vimrc" >> $HOME/.vimrc
-rm -rf $HOME/.vim
+rm -Rf $HOME/.wrkon
+mkdir $HOME/.wrkon
+rm -Rf $HOME/.vim
 cp -r $HOME/Base/config/vim/vimfiles $HOME/.vim
 source $HOME/.bashrc
