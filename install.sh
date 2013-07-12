d2ba1cb2cfeb4cf6f4d8fbbe33037cfb508895fd
root <root@debian-6.0.6-seed>
2013-07-12 20:41:33 +0000

    Changes

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@ echo $HOME
 
 echo "Naming machine $1"
 rm $HOME/.bashrc
-echo "source \$HOME/Base/config/bash/bashrc" >> $HOME/.bashrc
-echo "source \$HOME/Base/config/bash/bash_aliases" >> $HOME/.bashrc
-echo "source \$HOME/Base/config/bash/bash_commands" >> $HOME/.bashrc
+echo "export USR_HOME=$HOME" >> $HOME/.bashrc
+echo "source \$USR_HOME/Base/config/bash/bashrc" >> $HOME/.bashrc
+echo "source \$USR_HOME/Base/config/bash/bash_aliases" >> $HOME/.bashrc
+echo "source \$USR_HOME/Base/config/bash/bash_commands" >> $HOME/.bashrc
 #echo "alias wrkon='python \${HOME}/Base/wrkon/wrkon.py'" >> $HOME/.bashrc
 echo "export POST_PS1=\$PS1" >> $HOME/.bashrc
 echo "export MACH_NAME=$1" >> $HOME/.bashrc
 echo "export PS1=[$1]\$PS1" >> $HOME/.bashrc
-echo "export USR_HOME=$HOME" >> $HOME/.bashrc
 echo "source \$HOME/Base/config/vim/vimrc" >> $HOME/.vimrc
 rm -Rf $HOME/.wrkon
 mkdir $HOME/.wrkon
