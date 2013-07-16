7e23788edd9901e5ada4e7be3202822f153768dc
Robert THompson <robert.thompson@aunalytics.com>
2013-07-16 18:13:53 +0000

    Autocomplete

---
+++ config/vim/vim.py
@@ -0,0 +1,10 @@
+from py.au import au
+
+# au.flags.DEFINE_util("name", "Description")
+# au.flags.DEFINE_string("output_dir", None, "Output directory", required=True)
+
+def main(args):
+    pass
+
+if "__main__" == __name__:
+    au.run(main)
