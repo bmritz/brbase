54e530b39527878932bd672708acd819c97a4368
Robert Thompson <robert.thompson@aunalytics.com>
2013-09-20 10:46:11 -0500

    Allowing you to place this anywhere (i recommend ~/.base so it's hidden)

--- install.sh
+++ install.sh
@@ -2,24 +2,26 @@
 
 echo $HOME
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Naming machine $1"
 rm $HOME/.bashrc
 echo "export USR_HOME=$HOME" >> $HOME/.bashrc
-echo "source \$USR_HOME/Base/config/bash/bashrc" >> $HOME/.bashrc
-echo "source \$USR_HOME/Base/config/bash/bash_aliases" >> $HOME/.bashrc
-echo "source \$USR_HOME/Base/config/bash/bash_commands" >> $HOME/.bashrc
+echo "export BASE_DIR=$DIR" >> $HOME/.bashrc
+echo "source \$BASE_DIR/config/bash/bashrc" >> $HOME/.bashrc
+echo "source \$BASE_DIR/config/bash/bash_aliases" >> $HOME/.bashrc
+echo "source \$BASE_DIR/config/bash/bash_commands" >> $HOME/.bashrc
 #echo "alias wrkon='python \${HOME}/Base/wrkon/wrkon.py'" >> $HOME/.bashrc
 echo "export POST_PS1=\$PS1" >> $HOME/.bashrc
 echo "export MACH_NAME=$1" >> $HOME/.bashrc
 echo "export PS1=[$1]\$PS1" >> $HOME/.bashrc
 echo "export LD_LIBRARY_PATH=/opt/local/lib"
-echo "source \$USR_HOME/Base/config/vim/vimrc" >> $HOME/.vimrc
+echo "source \$BASE_DIR/config/vim/vimrc" >> $HOME/.vimrc
 #rm -Rf $HOME/.wrkon
 if [ ! -d "$HOME/.wrkon" ]
 then
   mkdir $HOME/.wrkon
 fi
-cp $HOME/Base/env.source $HOME/.wrkon
+cp $DIR/env.source $HOME/.wrkon
 rm -Rf $HOME/.vim
-cp -r $HOME/Base/config/vim/vimfiles $HOME/.vim
+cp -r $DIR/config/vim/vimfiles $HOME/.vim
 source $HOME/.bashrc
