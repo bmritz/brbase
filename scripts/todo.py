5ea89b445cdda8b01257ace175d4555c3f33706f
Robert Thompson <robert.thompson@aunalytics.com>
2013-10-30 11:05:26 -0400

    Adding in todo script to search files for TODO comments  ... can filter by your name

---
+++ scripts/todo.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+#
+# Todo.py: Searches files in directory for todo's with the specified username
+# To specify a todo in a file, type TODO(rft) or whatever username.
+#
+# Author: Robert Thompson
+#
+# Usage: python todo.py <user> <directory>
+#
+
+import sys
+import os
+import fnmatch
+import re
+
+def main():
+  assert len(sys.argv) >= 2, \
+    "Must have 3 arguments: todo <user> <dir>"
+  user = None
+
+  if len(sys.argv) == 2:
+      directory = sys.argv[1]
+  else:
+      user = sys.argv[1]
+      directory = sys.argv[2]
+
+
+  if user: print 'Searching directory %s for user %s' % (directory, user)
+  else: print 'Searching directory %s' % (directory)
+
+  matches=[]
+  for root, dirnames, filenames in os.walk(directory):
+    for filename in [n for n in filenames if match(n)]:
+      f = os.path.join(root,filename);
+      findTodos(f,user);
+
+def match(name):
+  types = ['py', 'js']
+  for t in types:
+    if fnmatch.fnmatch(name,'*.'+t):
+      return True
+  return False
+
+def findTodos(f,user):
+  count = 1
+  for line in open(f):
+    if re_match(line, user):
+      s = f+':'+str(count)+':'+line
+      print s,
+    count+=1
+
+def re_match(line, user):
+    if user: return re.search('TODO\('+user+'\)', line)
+    return re.search('TODO', line)
+
+
+if __name__ == '__main__':
+  main()
